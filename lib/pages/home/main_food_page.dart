b22b48e29b27fd6e08c8ed8e3d8c2d099a0c5cc8
lazharkarem <karem.lazhar@gmail.com>
2022-03-14 16:38:50 +0100

    update FoodPage:cleanup:orderBy_API

--- lib/pages/home/main_food_page.dart
+++ lib/pages/home/main_food_page.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/dimensions.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
+import 'package:get/get.dart';
 
+import '../../controllers/popular_product_controller.dart';
+import '../../controllers/recommended_product_controller.dart';
 import '../../utils/colors.dart';
 import 'food_page_body.dart';
 
@@ -16,63 +19,71 @@ class MainFoodPage extends StatefulWidget {
 }
 
 class _MainFoodPageState extends State<MainFoodPage> {
+  Future<void> _loadResource() async {
+    await Get.find<PopularProductController>().getPopularProductList();
+    await Get.find<RecommendedProductController>().getRecommendedProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      children: [
-        //Showing the header
-        Container(
-          child: Container(
-            margin: EdgeInsets.only(
-                top: Dimensions.height45, bottom: Dimensions.height15),
-            padding: EdgeInsets.only(
-                left: Dimensions.width20, right: Dimensions.width20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
+    return RefreshIndicator(
+        child: Scaffold(
+            body: Column(
+          children: [
+            //Showing the header
+            Container(
+              child: Container(
+                margin: EdgeInsets.only(
+                    top: Dimensions.height45, bottom: Dimensions.height15),
+                padding: EdgeInsets.only(
+                    left: Dimensions.width20, right: Dimensions.width20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    BigText(
-                      text: "Tunisie",
-                      color: AppColors.mainColor,
-                    ),
-                    Row(
+                    Column(
                       children: [
-                        SmallText(
-                          text: "Tunis",
-                          color: Colors.black54,
+                        BigText(
+                          text: "Tunisie",
+                          color: AppColors.mainColor,
                         ),
-                        Icon(Icons.arrow_drop_down_rounded)
+                        Row(
+                          children: [
+                            SmallText(
+                              text: "Tunis",
+                              color: Colors.black54,
+                            ),
+                            Icon(Icons.arrow_drop_down_rounded)
+                          ],
+                        )
                       ],
+                    ),
+                    Center(
+                      child: Container(
+                        width: Dimensions.height45,
+                        height: Dimensions.height45,
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                          size: Dimensions.iconSize24,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(Dimensions.radius15),
+                          color: AppColors.mainColor,
+                        ),
+                      ),
                     )
                   ],
                 ),
-                Center(
-                  child: Container(
-                    width: Dimensions.height45,
-                    height: Dimensions.height45,
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.white,
-                      size: Dimensions.iconSize24,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(Dimensions.radius15),
-                      color: AppColors.mainColor,
-                    ),
-                  ),
-                )
-              ],
+              ),
             ),
-          ),
-        ),
-        //showing the body
-        Expanded(
-            child: SingleChildScrollView(
-          child: FoodPageBody(),
-        ))
-      ],
-    ));
+            //showing the body
+            Expanded(
+                child: SingleChildScrollView(
+              child: FoodPageBody(),
+            ))
+          ],
+        )),
+        onRefresh: _loadResource);
   }
 }
