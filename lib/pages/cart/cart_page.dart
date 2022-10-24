78e342c2ed2d9a08a5daba24975e71f620e60e3c
karem lazhar <karem.lazhar@gmail.com>
2022-10-24 17:11:30 +0100

    update... payment Options_delivery

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -533,8 +533,13 @@ class CartPage extends StatelessWidget {
       Get.find<CartController>().clear();
       Get.find<CartController>().removeCartSharedPreference();
       Get.find<CartController>().addToHistory();
-      Get.offNamed(RouteHelper.getPaymentPage(
-          orderID, Get.find<UserController>().userModel!.id));
+
+      if (Get.find<OrderController>().paymentIndex == 0) {
+        Get.offNamed(RouteHelper.getOrderSuccessPage(orderID, "success"));
+      } else {
+        Get.offNamed(RouteHelper.getPaymentPage(
+            orderID, Get.find<UserController>().userModel!.id));
+      }
     } else {
       showCustomSnackBar(message);
     }
